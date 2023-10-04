7102c459ebf0b898d13cb295441cc3f204eb1d6b
tobiaspasinato <tobiasmarpasinato2005@gmail.com>
2023-10-04 17:36:41 -0300

    5438745598

---
+++ random/poo2.py
@@ -0,0 +1,18 @@
+class Libro:
+    def __init__(self, titulo, autor, año_publicacion) -> None:
+        self.titulo = titulo
+        self.autor = autor
+        self.año_publicacion = año_publicacion
+        pass
+    
+    def mostrar(self) -> str:
+        mensaje = f"Libro : {self.titulo}\nAutor: {self.autor}\nAño de publicación: {self.año_publicacion}"
+        return mensaje
+
+title = input("Titulo del libro? ")
+author = input("Cual es el autor? ")
+year = input("Cual es el año de publicación? ")
+
+libro_A = Libro(title, author, year)
+
+print(libro_A.mostrar())
