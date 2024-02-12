5af1d475260667eef10aabebfb2d181d1bec1c17
tobiaspasinato <tobiasmarpasinato2005@gmail.com>
2024-02-12 01:10:47 -0300

    0.0.9

--- random/poo3.py
+++ random/poo3.py
@@ -1,6 +1,8 @@
+import sqlite3
+
 def get_connetion():
     lista = []
-    conn = sql.connect("netflix_oscar.db")
+    conn = sqlite3.connect("netflix_oscar.db")
     cur = conn.cursor()
     lista.append()
     return conn, cur
