a86c7eefe7129d6f21cd39a18a3316ec12c098ad
tobiaspasinato <tobiasmarpasinato2005@gmail.com>
2024-02-17 19:13:21 -0300

    0.0.4

--- random/cam.py
+++ random/cam.py
@@ -1,4 +1,4 @@
-contador = 0
+"""contador = 0
 acumulador = 0
 cantidad_de_numeros = 5
 
@@ -9,4 +9,18 @@ while contador < cantidad_de_numeros:
 
 resultado = acumulador / cantidad_de_numeros
 
-print(f"El promedio es {resultado}")
+print(f"El promedio es {resultado}")"""
+
+lista = ["hola", 3, "asd", "jajaja", 3.14]
+lista2 = []
+contador = 0
+
+for elemento in lista:
+    if type(elemento) == int:
+        lista2.append(elemento)
+    else:
+        contador += 1
+
+for e in lista2:
+    print(e)
+print(contador)
