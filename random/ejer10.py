59987e23e7a72ac5ec1269555991eb4c570d4ded
tobiaspasinato <tobiasmarpasinato2005@gmail.com>
2024-04-14 23:36:28 -0300

    0-9

--- random/ejer10.py
+++ random/ejer10.py
@@ -1,1 +1,34 @@
-print(round(64.55))
+
+"""
+jugada_usuario = input("Elegi una jugada(T para tijera, R para piedra, P para papel): ")
+jugada_usuario = jugada_usuario.lower()
+if jugada_usuario == "t" or jugada_usuario == "r" or jugada_usuario == "p" or jugada_usuario == "goku":
+    match jugada_usuario:
+        case "t":
+            print("Te gane elegi R (Piedra)")
+        case "r":
+            print("Te gane elegi P (Papel)")
+        case "p":
+            print("Te gane elegi T (Tijera)")
+        case "goku":
+            print("Perdi, nada le gana a Goku")
+else:
+    print("Eso no es una jugada valida")
+"""
+def get_primo(num):
+    x = 1
+    c = 0
+    while x <= num:
+        if num % x == 0:
+            c += 1
+        x += 1
+    if c == 2:
+        return True
+    else:
+        return False
+
+rango = int(input("Cual es el rango de numero primos que queres mostrar? "))
+
+for i in range(1, rango):
+    if get_primo(i) == True:
+        print(i)
--- random/ejer9.py
+++ random/ejer9.py
@@ -1,6 +1,38 @@
-contraseña = "1234asd"
+producto1 = ("pollo", 600)
+producto2 = ("carne", 500)
+producto3 = ("queso", 200)
+producto4 = ("pan", 300)
+lista_productos = [producto1, producto2, producto3, producto4]
 
-nom = input("Contraseña :   ").lower()
+def tomar_pedido(lista : list) -> list:
+    on = True
+    lista_compra = []
+    while on:
+        eleccion = int(input("¿Qué desea comprar? (1 para detergente, 2 para jabon, 3 para esponja y 4 para trapo); 5 para salir "))
+        if eleccion == 5:
+            on = False
+        elif eleccion == 1:
+            var1 = lista_productos[0]
+            var2 = var1[1]
+            lista_compra.append(int(var2))
+        elif eleccion == 2:
+            var1 = lista_productos[1]
+            var2 = var1[1]
+            lista_compra.append(int(var2))
+        elif eleccion == 3:
+            var1 = lista_productos[2]
+            var2 = var1[1]
+            lista_compra.append(int(var2))
+        elif eleccion == 4:
+            var1 = lista_productos[3]
+            var2 = var1[1]
+            lista_compra.append(int(var2))
+    return lista_compra
 
-while nom != contraseña:
-    nom = input("Contraseña :   ").lower()
+def calcular_precio(lista : list) -> int:
+    acumulador_precio = 0
+    for i in lista:
+        acumulador_precio = acumulador_precio + i
+    return acumulador_precio
+
+print(f"El precio total es de {calcular_precio(tomar_pedido(lista_productos))}")
